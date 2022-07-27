7ea773900216d0ddfeabb23d149584bf53ba1dfa
oresho <somuyiwaoreoluwa@gmail.com>
2022-07-27 11:05:31 +0100

    Initial

---
+++ CompanyEmployees/Controllers/WeatherForecastController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Contracts;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace CompanyEmployees.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private ILoggerManager _logger;
+        private readonly IRepositoryManager _repository;
+
+        public WeatherForecastController(ILoggerManager logger, IRepositoryManager repository)
+        {
+            _logger = logger;
+            _repository = repository;
+        }
+
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            _logger.LogInfo("Here is info message from our values controller.");
+            _logger.LogDebug("Here is debug message from our values controller.");
+            _logger.LogWarn("Here is warn message from our values controller.");
+            _logger.LogError("Here is an error message from our values controller.");
+
+            return new string[] { "value1", "value2" };
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Getr()
+        {
+            //_repository.Company.AnyMethodFromCompanyRepository();
+            //_repository.Employee.AnyMethodFromEmployeeRepository();
+            return new string[] { "value1", "value2" };
+        }
+    }
+}
